2766e50ad58b75ce317b7a61b46a29333c9c37ff
jjauzion <jjauzion@student.42.fr>
2020-01-10 11:39:53 +0100

    auto install es

---
+++ es-auto-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.1-darwin-x86_64.tar.gz
+curl -O https://artifacts.elastic.co/downloads/kibana/kibana-7.5.1-darwin-x86_64.tar.gz
+tar -xzf elasticsearch-7.5.1-darwin-x86_64.tar.gz
+tar -xzf kibana-7.5.1-darwin-x86_64.tar.gz
