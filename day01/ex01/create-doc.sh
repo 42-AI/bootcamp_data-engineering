46acd09f977e92ff9e4fd8fc365e83389d0f6729
jjauzion <jjauzion@student.42.fr>
2020-01-10 17:13:54 +0100

    ex01 v1 done

--- day01/ex01/create-doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-curl -X POST "http://localhost:9200/twitter/_doc/?pretty" -H 'Content-Type: application/json' -d'
-{
-    "user" : "popol",
-    "post_date" : "2009-11-15",
-    "message" : "still trying out Elasticsearch"
-}'
