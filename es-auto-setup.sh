fc9ea1aaa765eeca20fec2ffb81efe272a20ae43
jjauzion <jjauzion@student.42.fr>
2020-01-10 13:10:32 +0100

    add correction to ex00

--- es-auto-setup.sh
+++ es-auto-setup.sh
@@ -4,3 +4,5 @@ curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.
 curl -O https://artifacts.elastic.co/downloads/kibana/kibana-7.5.1-darwin-x86_64.tar.gz
 tar -xzf elasticsearch-7.5.1-darwin-x86_64.tar.gz
 tar -xzf kibana-7.5.1-darwin-x86_64.tar.gz
+echo 'cluster.name: "my-cluster"' > elasticsearch-7.5.1/config/elasticsearch.yml
+echo 'node.name: "node1"' >> elasticsearch-7.5.1/config/elasticsearch.yml
