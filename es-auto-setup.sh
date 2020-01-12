2052c7c82b2407502498a759abb20a16e2a3e1ae
jjauzion <jjauzion@student.42.fr>
2020-01-12 20:11:01 +0100

    adding logstash to es-setup

--- es-auto-setup.sh
+++ es-auto-setup.sh
@@ -6,3 +6,6 @@ tar -xzf elasticsearch-7.5.1-darwin-x86_64.tar.gz
 tar -xzf kibana-7.5.1-darwin-x86_64.tar.gz
 echo 'cluster.name: "my-cluster"' > elasticsearch-7.5.1/config/elasticsearch.yml
 echo 'node.name: "node1"' >> elasticsearch-7.5.1/config/elasticsearch.yml
+curl -O https://artifacts.elastic.co/downloads/logstash/logstash-7.5.1.tar.gz
+tar -xvf logstash-7.5.1.tar.gz
+cp ~/42/bootcamp_data-engineering/day01/ingest-pipeline.conf logstash-7.5.1/config/
