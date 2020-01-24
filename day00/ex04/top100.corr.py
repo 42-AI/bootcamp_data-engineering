89b77b26043d92ba54efe0933388eb276fb85c2a
Francois-Xavier <fxbabin@outlook.fr>
2020-01-24 23:29:17 +0100

    refactor: enforced use of postgresql only in ex04

--- day00/ex04/top100.corr.py
+++ day00/ex04/top100.corr.py
@@ -6,25 +6,26 @@ import psycopg2
 from psycopg2.extensions import AsIs
 import re
 
-def get_top_100():
+def get_connection():
     conn = psycopg2.connect(
         database="appstore_games",
         host="localhost",
         user="postgres_user",
         password="12345"
     )
+    return (conn)
+
+def get_top_100():
+    conn = get_connection()
     curr = conn.cursor()
     curr.execute(""" SELECT Name, Avg_user_rating
-                     FROM appstore_games 
+                     FROM appstore_games
+                     WHERE Name !~ '^[0-9]'
                      ORDER BY Avg_user_rating DESC, Name
+                     LIMIT 100
                  """)
     response = curr.fetchall()
-    count = 0
     for row in response:
-        if not re.match(r'^[0-9]*', row[0])[0]:
-            count += 1
-            print(row[0])
-        if count == 100:
-            break
+        print(row[0])
     conn.close()
 get_top_100()
