e08abb1baa8cde275eb9662a016647ef89e1a27c
Francois-Xavier <fxbabin@outlook.fr>
2020-01-25 21:33:00 +0100

    refactor: ex08 pep8 norm

--- day00/ex08/battle_royale.corr.py
+++ day00/ex08/battle_royale.corr.py
@@ -5,6 +5,7 @@
 import psycopg2
 from psycopg2.extensions import AsIs
 
+
 def get_connection():
     conn = psycopg2.connect(
         database="appstore_games",
@@ -14,16 +15,25 @@ def get_connection():
     )
     return (conn)
 
+
 def get_battle_royale():
     conn = get_connection()
     curr = conn.cursor()
     curr.execute(""" SELECT Name, Description
                      FROM appstore_games
                      WHERE Description ILIKE '%battle_royale%'
-                     AND Description ~ '.*https?://(www\.)?(facebook|fb)\.(com).*';
+                     AND Description ~
+                     '.*https?://(www.)?(facebook|fb).(com).*';
                  """)
     response = curr.fetchall()
     for row in response:
         print(row[0])
     conn.close()
-get_battle_royale()
+
+
+def main():
+    get_battle_royale()
+
+
+if __name__ == "__main__":
+    main()
