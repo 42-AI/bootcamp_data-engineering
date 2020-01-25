b79c1b16b584406ddac50f9846e2a91c33ea6b36
Francois-Xavier <fxbabin@outlook.fr>
2020-01-25 20:30:55 +0100

    refactor: ex03 at pep8 norm

--- day00/ex03/populate.corr.py
+++ day00/ex03/populate.corr.py
@@ -6,6 +6,7 @@ import pandas as pd
 import psycopg2
 from psycopg2.extensions import AsIs
 
+
 ######################
 #   SET CONNECTION   #
 ######################
@@ -19,25 +20,27 @@ def get_connection():
     )
     return (conn)
 
+
 ####################
 #   DELETE TABLE   #
 ####################
 
 def delete_table(table: str):
-    print("\tdeleting table '{}'".format(table),end='')
-    conn = set_connection()
+    print("\tdeleting table '{}'".format(table), end='')
+    conn = get_connection()
     curr = conn.cursor()
     curr.execute("DROP TABLE IF EXISTS %(table)s;", {"table": AsIs(table)})
     conn.commit()
     conn.close()
     print(' done !')
 
+
 #####################
 #   DISPLAY TABLE   #
 #####################
 
 def display_table(table: str):
-    conn = set_connection()
+    conn = get_connection()
     curr = conn.cursor()
     curr.execute("SELECT * FROM %(table)s LIMIT 10", {"table": AsIs(table)})
     response = curr.fetchall()
@@ -45,13 +48,14 @@ def display_table(table: str):
         print(row)
     conn.close()
 
+
 ##############################
 #   CREATE TABLE FUNCTIONS   #
 ##############################
 
 def create_table_appstore_games():
-    print("\tcreating table 'appstore_games' ...",end='')
-    conn = set_connection()
+    print("\tcreating table 'appstore_games' ...", end='')
+    conn = get_connection()
     curr = conn.cursor()
     curr.execute("""CREATE TABLE IF NOT EXISTS appstore_games(
              Game_Id bigint PRIMARY KEY,
@@ -70,9 +74,10 @@ def create_table_appstore_games():
     conn.close()
     print(' done !')
 
+
 def create_table_appstore_games_languages():
-    print("\tcreating table 'appstore_games_languages' ...",end='')
-    conn = set_connection()
+    print("\tcreating table 'appstore_games_languages' ...", end='')
+    conn = get_connection()
     curr = conn.cursor()
     curr.execute("""CREATE TABLE IF NOT EXISTS appstore_games_languages(
              Id bigint PRIMARY KEY,
@@ -85,8 +90,8 @@ def create_table_appstore_games_languages():
 
 
 def create_table_appstore_games_genres():
-    print("\tcreating table 'appstore_games_genres' ...",end='')
-    conn = set_connection()
+    print("\tcreating table 'appstore_games_genres' ...", end='')
+    conn = get_connection()
     curr = conn.cursor()
     curr.execute("""CREATE TABLE IF NOT EXISTS appstore_games_genres(
              Id bigint PRIMARY KEY,
@@ -98,19 +103,20 @@ def create_table_appstore_games_genres():
     conn.close()
     print(' done !')
 
+
 ##########################
 #   POPULATE FUNCTIONS   #
 ##########################
-    
+
 def populate_appstore_games(df):
-    print("\tpopulating table 'appstore_games' ...",end='')
-    conn = set_connection()
+    print("\tpopulating table 'appstore_games' ...", end='')
+    conn = get_connection()
     curr = conn.cursor()
     for idx in range(df.shape[0]):
         tmp = df.iloc[idx]
         curr.execute("""INSERT INTO appstore_games
             (Game_Id,
-            Name, 
+            Name,
             Avg_user_rating,
             User_rating_count,
             Price,
@@ -119,27 +125,28 @@ def populate_appstore_games(df):
             Age_rating,
             Size,
             Release_date,
-            Last_update) VALUES 
+            Last_update) VALUES
             (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-            (int(tmp["ID"]), 
-             tmp["Name"],
-             tmp["Average User Rating"],
-             int(tmp["User Rating Count"]),
-             tmp["Price"],
-             tmp["Description"],
-             tmp["Developer"],
-             int(tmp["Age Rating"]),
-             int(tmp["Size"]),
-             tmp["Original Release Date"],
-             tmp["Current Version Release Date"]
-            ))
+                     (int(tmp["ID"]),
+                      tmp["Name"],
+                      tmp["Average User Rating"],
+                      int(tmp["User Rating Count"]),
+                      tmp["Price"],
+                      tmp["Description"],
+                      tmp["Developer"],
+                      int(tmp["Age Rating"]),
+                      int(tmp["Size"]),
+                      tmp["Original Release Date"],
+                      tmp["Current Version Release Date"]
+                      ))
     conn.commit()
     conn.close()
     print(' done !')
 
+
 def populate_appstore_games_genres(df):
-    print("\tpopulating table 'appstore_games_genres' ...",end='')
-    conn = set_connection()
+    print("\tpopulating table 'appstore_games_genres' ...", end='')
+    conn = get_connection()
     curr = conn.cursor()
     for idx in range(df.shape[0]):
         tmp = df.iloc[idx]
@@ -147,39 +154,42 @@ def populate_appstore_games_genres(df):
             (Id,
             Game_Id,
             Primary_genre,
-            Genre) VALUES 
+            Genre) VALUES
             (%s, %s, %s, %s)""",
-            (idx,
-             int(tmp["ID"]),
-             tmp["Primary Genre"],
-             tmp["Genres"]
-            ))
+                     (idx,
+                      int(tmp["ID"]),
+                      tmp["Primary Genre"],
+                      tmp["Genres"]
+                      ))
     conn.commit()
     conn.close()
     print(' done !')
-    
+
+
 def populate_appstore_games_languages(df):
-    print("\tpopulating table 'appstore_games_languages' ...",end='')
-    conn = set_connection()
+    print("\tpopulating table 'appstore_games_languages' ...", end='')
+    conn = get_connection()
     curr = conn.cursor()
     for idx in range(df.shape[0]):
         tmp = df.iloc[idx]
         curr.execute("""INSERT INTO appstore_games_languages
             (Id,
             Game_Id,
-            Language) VALUES 
+            Language) VALUES
             (%s, %s, %s)""",
-            (idx,
-             int(tmp["ID"]),
-             tmp["Languages"]
-            ))
+                     (idx,
+                      int(tmp["ID"]),
+                      tmp["Languages"]
+                      ))
     conn.commit()
     conn.close()
     print(' done !')
 
+
 ############
 #   MAIN   #
 ############
+
 
 def main():
     print("IMPORTING CSVS ...")
@@ -187,10 +197,12 @@ def main():
     print("\timporting '{}'".format('appstore_games.normalized.csv'), end='')
     df = pd.read_csv("appstore_games.normalized.csv")
     print(' done !')
-    print("\timporting '{}'".format('appstore_games_genres.normalized.csv'), end='')
+    print("\timporting '{}'".format(
+        'appstore_games_genres.normalized.csv'), end='')
     df_genres = pd.read_csv("appstore_games_genres.normalized.csv")
     print(' done !')
-    print("\timporting '{}'".format('appstore_games_languages.normalized.csv'), end='')
+    print("\timporting '{}'".format(
+        'appstore_games_languages.normalized.csv'), end='')
     df_languages = pd.read_csv("appstore_games_languages.normalized.csv")
     print(' done !')
 
@@ -213,6 +225,7 @@ def main():
     populate_appstore_games_languages(df_languages)
 
     print('ALL DONE !')
+
 
 if __name__ == "__main__":
     main()
