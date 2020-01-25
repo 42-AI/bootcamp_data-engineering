f86491fc443e769dfc5d342a10dd85e858b7a6bc
Francois-xavier BABIN <fbabin@e3r10p18.42.fr>
2020-01-25 18:06:30 +0100

    refactor: ex02 flake8 norm

--- day00/ex02/normalize.corr.py
+++ day00/ex02/normalize.corr.py
@@ -2,8 +2,10 @@
 #   Ex02: Normalize   #
 #######################
 
+
 import re
 import pandas as pd
+
 
 def nf_normalization_genres(df):
     """Get normalized genres dataframe.
@@ -15,10 +17,13 @@ def nf_normalization_genres(df):
       This function shouldn't raise any Exception.
     """
     df_genres = df[["ID", "Primary Genre", "Genres"]].copy()
-    df_genres["Genres"] = df_genres["Genres"].apply(lambda x: re.split(r'[ ,]+', x))
+    df_genres["Genres"] = (
+      df_genres["Genres"].apply(lambda x: re.split(r'[ ,]+', x))
+    )
     df_genres = df_genres.explode('Genres')
     df_genres = df_genres.reset_index(drop=True)
     return(df_genres)
+
 
 def nf_normalization_languages(df):
     """Get normalized languages dataframe.
@@ -29,18 +34,28 @@ def nf_normalization_languages(df):
     Raises:
       This function shouldn't raise any Exception.
     """
-    df_languages = df[["ID","Languages"]].copy()
-    df_languages["Languages"] = df_languages["Languages"].apply(lambda x: re.split(r'[ ,]+', x))
+    df_languages = df[["ID", "Languages"]].copy()
+    df_languages["Languages"] = (
+      df_languages["Languages"].apply(lambda x: re.split(r'[ ,]+', x))
+    )
     df_languages = df_languages.explode('Languages')
     df_languages = df_languages.reset_index(drop=True)
     return(df_languages)
 
-df = pd.read_csv("../ex01/appstore_games.cleaned.csv")
 
-df_genres = nf_normalization_genres(df)
-df_languages = nf_normalization_languages(df)
-df = df.drop(['Languages', 'Primary Genre', 'Genres'], axis=1)
+def main():
+    df = pd.read_csv("../ex01/appstore_games.cleaned.csv")
 
-df.to_csv("appstore_games.normalized.csv", index=False)
-df_genres.to_csv("appstore_games_genres.normalized.csv", index=False)
-df_languages.to_csv("appstore_games_languages.normalized.csv", index=False)
+    # normalize dataframes
+    df_genres = nf_normalization_genres(df)
+    df_languages = nf_normalization_languages(df)
+    df = df.drop(['Languages', 'Primary Genre', 'Genres'], axis=1)
+
+    # saving dataframes into csv files
+    df.to_csv("appstore_games.normalized.csv", index=False)
+    df_genres.to_csv("appstore_games_genres.normalized.csv", index=False)
+    df_languages.to_csv("appstore_games_languages.normalized.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
