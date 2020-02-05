67bccbda2ec5222ec2c58f777772fa5351b60829
Francois-xavier BABIN <fbabin@e2r5p6.42.fr>
2020-02-05 21:59:50 +0100

    feat: improved ex00 correction with boilerplate

--- day00/ex00/setup.corr.py
+++ day00/ex00/setup.corr.py
@@ -3,80 +3,93 @@
 ###################
 
 import getpass
+import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+###################
+#   BOILERPLATE   #
+###################
 
-def get_default_connection():
-    conn = psycopg2.connect(
-        dbname='postgres',
-        user=getpass.getuser(),
-        host='',
-        password="12345"
-    )
-    return (conn)
+
+def run_sql(query: str,
+            db="postgres",
+            passwd="12345",
+            fetch=False,
+            commit=False):
+    res = ""
+
+    # Connection to the database
+    try:
+        conn = psycopg2.connect(
+            dbname=db,
+            user=getpass.getuser(),
+            host='',
+            password=passwd
+        )
+    except Exception as e:
+        print("Connection Error :: {}".format(str(e).strip()))
+        return(res)
+    if commit:
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    curr = conn.cursor()
+
+    # Run sql query
+    try:
+        curr.execute("{}".format(query))
+    except Exception as e:
+        print("Query Error :: {}".format(str(e).strip()))
+
+    # Fetch results
+    if fetch:
+        res = curr.fetchall()
+    if commit:
+        conn.commit()
+    conn.close()
+    return (res)
+
+
+#################
+#   FUNCTIONS   #
+#################
 
 
 def create_database(db: str):
-    conn = get_default_connection()
-    # AUTOCOMMIT allows database changes in python
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curr = conn.cursor()
-    curr.execute("CREATE DATABASE {}".format(db, db))
-    conn.commit()
-    conn.close()
+    print("Creating '{}' database ... ".format(db), end="")
+    run_sql("CREATE DATABASE {}".format(db), commit=True)
+    print("Done !")
 
 
 def drop_database(db: str):
-    conn = get_default_connection()
-    # AUTOCOMMIT allows database changes in python
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curr = conn.cursor()
-    curr.execute("DROP DATABASE IF EXISTS {}".format(db))
-    conn.commit()
-    conn.close()
+    print("Dropping '{}' database ... ".format(db), end="")
+    run_sql("DROP DATABASE IF EXISTS {}".format(db), commit=True)
+    print("Done !")
 
 
-def create_pg_user(user: str, password: str):
-    conn = get_default_connection()
-    # AUTOCOMMIT allows database changes in python
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curr = conn.cursor()
-    curr.execute("CREATE USER {} LOGIN PASSWORD '{}'".format(user, password))
-    conn.commit()
-    conn.close()
+def create_user(user: str, password: str):
+    print("Creating '{}' user ... ".format(user), end="")
+    run_sql("CREATE USER {} LOGIN PASSWORD '{}'".format(user, password),
+            commit=True)
+    print("Done !")
 
 
-def drop_pg_user(user: str):
-    conn = get_default_connection()
-    # AUTOCOMMIT allows database changes in python
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curr = conn.cursor()
-    curr.execute("DROP USER IF EXISTS {}".format(user))
-    conn.commit()
-    conn.close()
+def drop_user(user: str):
+    print("Dropping '{}' user ... ".format(user), end="")
+    run_sql("DROP USER IF EXISTS {}".format(user), commit=True)
+    print("Done !")
 
 
 def alter_database(db: str, user: str):
-    conn = get_default_connection()
-    # AUTOCOMMIT allows database changes in python
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    curr = conn.cursor()
-    curr.execute("ALTER DATABASE {} OWNER TO {}".format(db, user))
-    conn.commit()
-    conn.close()
+    print("Altering '{}' database, owner is '{}' ... ".format(db, user),
+          end="")
+    run_sql("ALTER DATABASE {} OWNER TO {}".format(db, user), commit=True)
+    print("Done !")
 
 
 def main():
-    print("Drop 'appstore_games' database")
     drop_database("appstore_games")
-    print("Create 'appstore_games' database")
     create_database("appstore_games")
-    print("Drop user '{}'".format("postgres_user"))
-    drop_pg_user("postgres_user")
-    print("Create user '{}'".format("postgres_user"))
-    create_pg_user("postgres_user", "12345")
-    print("Give '{}' database ownership to '{}'".format(
-        "appstore_games", "postgres_user"))
+    drop_user("postgres_user")
+    create_user("postgres_user", "12345")
     alter_database("appstore_games", "postgres_user")
 
 
