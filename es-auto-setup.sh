a0bede76d17cc47f96363d7a3543fad264313266
jjauzion <jjauzion@student.42.fr>
2020-02-07 09:29:26 +0100

    edit es auto setup

--- es-auto-setup.sh
+++ es-auto-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.5.1-darwin-x86_64.tar.gz
-curl -O https://artifacts.elastic.co/downloads/kibana/kibana-7.5.1-darwin-x86_64.tar.gz
 tar -xzf elasticsearch-7.5.1-darwin-x86_64.tar.gz
-tar -xzf kibana-7.5.1-darwin-x86_64.tar.gz
 echo 'cluster.name: "my-cluster"' > elasticsearch-7.5.1/config/elasticsearch.yml
 echo 'node.name: "node1"' >> elasticsearch-7.5.1/config/elasticsearch.yml
+curl -O https://artifacts.elastic.co/downloads/kibana/kibana-7.5.1-darwin-x86_64.tar.gz
+tar -xzf kibana-7.5.1-darwin-x86_64.tar.gz
 curl -O https://artifacts.elastic.co/downloads/logstash/logstash-7.5.1.tar.gz
 tar -xvf logstash-7.5.1.tar.gz
 cp ~/42/bootcamp_data-engineering/day01/ingest-pipeline.conf logstash-7.5.1/config/
