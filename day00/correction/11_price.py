1a06273549aff4a577e030c946983cddcd0968e1
Francois-Xavier Babin <fxbabin@Francois-Xaviers-MacBook-Pro.local>
2020-02-08 00:03:50 +0100

    refactor: improved ex11

--- day00/correction/11_price.py
+++ day00/correction/11_price.py
@@ -2,50 +2,32 @@
 #   Ex11: Price   #
 ###################
 
-import psycopg2
-from psycopg2.extensions import AsIs
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-def get_connection():
-    conn = psycopg2.connect(
-        database="appstore_games",
-        host="localhost",
-        user="postgres_user",
-        password="12345"
-    )
-    return (conn)
+from correction.utils.utils import run_sql
 
 
 def get_price():
-    conn = get_connection()
-    curr = conn.cursor()
-    curr.execute(""" SELECT Price, COUNT(Price)
-                     FROM appstore_games
-                     GROUP BY Price
-                     ORDER BY Price
-                 """)
-    response = curr.fetchall()
-    response = sorted(response)
-    data = []
-    for e in response:
-        for _ in range(e[1]):
-            data.append(e[0])
-    print("mean price : ", np.mean(data))
-    print("std price : ", np.std(data))
-    data = [e for e in data if e > 0]
-    bins = []
-    for i in range(0, 200, 3):
-        bins.append(i)
-    plt.hist(data, bins)
+    res= run_sql("""
+                    SELECT Price, COUNT(Price)
+                    FROM appstore_games
+                    WHERE Price > 1
+                    GROUP BY Price
+                    ORDER BY Price
+                 """, fetch=True)
+    d = []
+    for k, v in res:
+        d += [k] * v
+    print("mean price : ", np.mean(d))
+    print("std price : ", np.std(d))
+    bins = [i for i in range(0, 200, 3)]
+    plt.hist([e[1] for e in res], bins)
     plt.xlabel("Price")
     plt.ylabel("Frequency")
     plt.title("Appstore games price")
     plt.savefig("price.png")
     plt.show()
-    conn.close()
 
 
 def main():
