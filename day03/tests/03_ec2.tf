d3da4f66843c0fa36445e91d03b71998b9d35512
Francois-Xavier <fxbabin@outlook.fr>
2020-09-19 15:04:20 +0200

    fix: fixed wordpress bootstrap

--- day03/tests/03_ec2.tf
+++ day03/tests/03_ec2.tf
@@ -39,14 +39,12 @@ resource "aws_instance" "test" {
         aws_db_instance.mysql_wordpress
     ]
 
-
  ami                         = data.aws_ami.amazon-linux-2.id
  associate_public_ip_address = true
  instance_type               = "t2.micro"
  key_name                    = "ddd"
  vpc_security_group_ids      = [aws_security_group.day03_wp_sg.id]
  subnet_id                   = aws_subnet.day03_sn_1.id
-
  user_data = data.template_file.init.rendered
 
 }
