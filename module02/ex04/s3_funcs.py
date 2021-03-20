d138b1c88ae0fe89d041571e0b70893578a811dd
fxbabin <fxbabin@outlook.fr>
2021-03-20 21:34:24 +0100

    Add correction for exercise04

---
+++ module02/ex04/s3_funcs.py
@@ -0,0 +1,25 @@
+import boto3
+
+def list_files(bucket):
+    """
+    Function to list files in a given S3 bucket
+    """
+    s3_client = boto3.client('s3')
+    contents = []
+    for item in s3_client.list_objects(Bucket=bucket)['Contents']:
+        contents.append(item['Key'])
+    return contents
+
+def upload_file(file_name, bucket):
+    """
+    Function to upload a file to an S3 bucket
+    """
+    s3_client = boto3.client('s3')
+    return s3_client.generate_presigned_post(bucket, file_name)
+
+def delete_file(file_name, bucket):
+    """
+    Function to delete a file to an S3 bucket
+    """
+    s3_client = boto3.client('s3')
+    return s3_client.delete_object(Bucket=bucket, Key=file_name)
