1d5c720b496d7f5e22d74e379448e169a5343712
fxbabin <fxbabin@outlook.fr>
2021-03-20 18:00:17 +0100

    Add correction for module02 ex03

--- module02/ex03/app.py
+++ module02/ex03/app.py
@@ -1,23 +1,34 @@
+#!/usr/bin/env python3
+
+# ============================================================================#
+# =============================== LIBRARIES ==================================#
+# ============================================================================#
+
 from flask import Flask, request, send_file
 from s3_funcs import list_files
+
+# ============================================================================#
+# ================================ FUNCTIONS =================================#
+# ============================================================================#
 
 app = Flask(__name__)
 BUCKET = "module02-12345"
 
 @app.route('/')
 def home():
-    response = {}
-    response['status'] = "200"
-    response['message'] = "Successfully connected to module03 cloud storage API"
+    response = {
+        'status': '200',
+        'message': 'Successfully connected to module02 cloud storage API'
+    }
     return response
-
 
 @app.route("/list_files")
 def list_bucket():
-    response = {}
-    response['status'] = "200"
-    response['message'] = "Successfully listed files on s3 bucket '{}'.".format(BUCKET)
-    response['content'] = list_files(BUCKET)
+    response = {
+        'status': '200',
+        'message': "Successfully listed files on s3 bucket '{}'.".format(BUCKET),
+        'content': list_files(BUCKET)        
+    }
     return response
 
 
