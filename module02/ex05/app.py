e422119ffbd04de382a4d5994da9886a15cb1bca
fxbabin <fxbabin@outlook.fr>
2021-03-21 23:25:07 +0100

    Add correction for ex05

---
+++ module02/ex05/app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# ============================================================================#
+# =============================== LIBRARIES ==================================#
+# ============================================================================#
+
+from flask import Flask, request, send_file
+from s3_funcs import list_files, upload_file, delete_file
+
+# ============================================================================#
+# ================================ FUNCTIONS =================================#
+# ============================================================================#
+
+app = Flask(__name__)
+BUCKET = "module02-12345"
+
+@app.route('/')
+def home():
+    response = {
+        'status': 200,
+        'message': 'Successfully connected to module02 cloud storage API'
+    }
+    return response
+
+@app.route("/list_files")
+def list_bucket():
+    response = {
+        'status': 200,
+        'message': "Successfully listed files on s3 bucket '{}'.".format(BUCKET),
+        'content': list_files(BUCKET)        
+    }
+    return response
+
+@app.route('/upload/<filename>', methods= ['GET', 'POST'])
+def upload(filename):
+    if request.method == 'GET':
+        response = {
+            'status': 200,
+            'message': "Successfully uploaded file '{}' on s3 bucket '{}'".format(filename, BUCKET),
+            'content': upload_file(filename, BUCKET)
+        }
+        return (response)
+
+@app.route('/delete/<filename>', methods= ['GET', 'POST'])
+def delete(filename):
+    if request.method == 'GET':
+        response = {
+            'status': 200,
+            'message': "Successfully deleted file '{}' on s3 bucket '{}'".format(filename, BUCKET),
+            'content': delete_file(filename, BUCKET)
+        }
+        return (response)
+
+@app.route("/download/<filename>", methods=['GET'])
+def download(filename):
+    # response = {}
+    if request.method == 'GET':
+        presigned_url = download_file(filename, BUCKET)
+        print(presigned_url)
+        response = {
+            'status': 200,
+            'message': "Successfully downloaded file '{}' on s3 bucket '{}'".format(filename, BUCKET),
+            'content': presigned_url
+        }
+        return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ module02/ex05/client.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# ============================================================================#
+# =============================== LIBRARIES ==================================#
+# ============================================================================#
+
+import sys
+import click
+import requests
+
+# ============================================================================#
+# ================================ FUNCTIONS =================================#
+# ============================================================================#
+
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option('--ip', default='0.0.0.0', help="Ip of the API")
+def ping(ip):
+    r = requests.get("http://{}:5000/".format(ip)).json()
+    if r['status'] == 200:
+        print(r['message'])
+
+@click.command()
+@click.option('--ip', default='0.0.0.0', help="Ip of the API")
+def list(ip):
+    r = requests.get("http://{}:5000/list_files".format(ip)).json()
+    if r['status'] == 200:
+        for e in r['content']:
+            print(e)
+
+@click.command()
+@click.option('--ip', default='0.0.0.0', help="Ip of the API")
+@click.option('--filename', help="File")
+def upload(ip, filename):
+    r = requests.get("http://{}:5000/upload/{}".format(ip, filename)).json()
+    if r['status'] == 200:
+        with open(filename, 'rb') as file_to_upload:
+            files = {'file': (filename, file_to_upload)}
+            upload_response = requests.post(r['content']['url'], data=r['content']['fields'], files=files)
+            if upload_response.status_code == 204:
+                print(r['message'])
+
+@click.command()
+@click.option('--ip', default='0.0.0.0', help="Ip of the API")
+@click.option('--filename', help="File")
+def delete(ip, filename):
+    r = requests.get("http://{}:5000/delete/{}".format(ip, filename)).json()
+    if r['status'] == 200:
+        print(r['message'])
+
+@click.command()
+@click.option('--ip', default='0.0.0.0', help="Ip of the API")
+@click.option('--filename', help="File")
+def download(ip, filename):
+    r = requests.get("http://{}:5000/upload/{}".format(ip, filename)).json()
+    if r['status'] == 200:
+        with open(filename, 'rb') as file_to_upload:
+            files = {'file': (filename, file_to_upload)}
+            upload_response = requests.post(r['content']['url'], data=r['content']['fields'], files=files)
+            if upload_response.status_code == 204:
+                print(r['message'])
+
+cli.add_command(ping)
+cli.add_command(list)
+cli.add_command(upload)
+cli.add_command(delete)
+
+if __name__ == "__main__":
+    cli()
